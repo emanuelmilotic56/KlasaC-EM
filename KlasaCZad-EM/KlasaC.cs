a0e28b92748408d5a427568eabd3c10023d6f98d
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-12-10 20:20:50 +0100

    Add files via upload

---
+++ KlasaCZad-EM/KlasaC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaCZad_EM
+{
+    class KlasaC
+    {
+
+        public int Zbroj(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+                sum += arr[i];
+            return sum;
+        }
+        public double Prosjek(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+                sum += arr[i];
+            return (double)sum / arr.Length;
+        }
+    }
+
+}
---
+++ KlasaCZad-EM/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaCZad_EM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[5];
+
+            Console.WriteLine("Upišite 5 cijelih brojeva:");
+            for (int i = 0; i < 5; i++)
+            {
+                string s = Console.ReadLine();
+                arr[i] = Convert.ToInt32(s);
+            }
+            KlasaC x = new KlasaC();
+            int zbroj = x.Zbroj(arr);
+            Console.WriteLine(zbroj);
+            double prosjek = x.Prosjek(arr);
+            Console.WriteLine(prosjek);
+
+        }
+    }
+}
